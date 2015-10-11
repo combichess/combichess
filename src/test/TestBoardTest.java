82f3005c9a5c0eec876b4d3768b8b001faee5997
combichess <mattias.alm@combitech.se>
2015-10-11 20:59:58 +0200

    fattar inte vad som händer

--- src/test/TestBoardTest.java
+++ src/test/TestBoardTest.java
@@ -5,10 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-<<<<<<< HEAD
-=======
-import system.board.Board;
->>>>>>> 59afa744f20acdbbb3fe590df4aa59a5b2f46416
 import system.board.PlayerColour;
 import system.move.Move;
 import system.piece.PieceType;
@@ -69,72 +65,4 @@ public class TestBoardTest {
 	{
 		
 	}
-<<<<<<< HEAD
 }
-=======
-	
-	
-	
-		// overriding Board to test Board
-	public class BoardTester extends Board
-	{
-		BoardTester() {
-			super();
-			
-		}
-		
-		public void clearAllData()
-		{
-			clearSetup();
-		}
-		
-		public void addPiece(int xPos, int yPos, PlayerColour player, PieceType type)
-		{
-			super.addPiece(xPos, yPos, player, type);
-		}
-		
-		public Move findBestMoveFor(PlayerColour col, int N)
-		{
-			return super.findBestMoveFor(col, N);
-		}
-	}
-}
-
-
-/*
- * 	// ta tiden
-		
-		//Board bord = new Board(white, black);
-		addPiece(4, 2, PlayerColour.Black, PieceType.King);
-		addPiece(4, 3, PlayerColour.White, PieceType.Pawn);
-		addPiece(2, 4, PlayerColour.Black, PieceType.Knight);
-		addPiece(4, 5, PlayerColour.White, PieceType.Knight);
-		addPiece(5, 5, PlayerColour.White, PieceType.King);
-		addPiece(5, 7, PlayerColour.White, PieceType.Queen);
-		
-		
-		// ser svart ett steg fram så är det bäst att ta hästen.
-		// ser svart två steg fram så tar han bonden
-		System.out.println(this);
-		System.out.println((!fullTest()? "FAILED !!!": "passed") + " på test 01.a");
-
-		
-		Move moveB = findBestMoveFor(black, 1);
-		System.out.println(((moveB == null || moveB.getXmove() != 2 || moveB.getYmove() != 1)? "FAILED !!!": "passed") + " på test 01.b\t" + moveB);
-
-		moveB = findBestMoveFor(black, 2);
-		System.out.println(((moveB == null || moveB.getXmove() != 2 || moveB.getYmove() != -1)? "FAILED !!!": "passed") + " på test 01.c\t" + moveB);
-		System.out.println((!fullTest()? "FAILED !!!": "passed") + " på test 01.d");
-
-		System.out.println(this);
-			// I detta läge ska svart kunna se att den kan göra ett gaffelhot mot kung och dam.
-			// Vit måste kunna ränka ut att den då måste välja att flytta kungen
-			// Därför är rätt svar: BNc5 -> d7
-		moveB = findBestMoveFor(black, 3);
-		System.out.println(((moveB == null || moveB.getXmove() != 1 || moveB.getYmove() != 2)? "FAILED !!!": "passed") + " på test 01.e\t" + moveB);
-		System.out.println();
-		System.out.println((!fullTest()? "FAILED !!!": "passed") + " på test 01.f");
-		
-		return true;
- * */
->>>>>>> 59afa744f20acdbbb3fe590df4aa59a5b2f46416
